65376c7df580c7a6386e559f596800ae619e804b
ArumetaM <matsuge.super.great@gmail.com>
2018-10-30 19:17:41 +0900

    Success

---
+++ ABC112A.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    N = int(input())
+    if N == 1:
+        print("Hello World")
+        exit()
+    else:
+        A = int(input())
+        B = int(input())
+        print(A+B)
