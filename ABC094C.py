71294bf05d7322000ddaa6735e1ca45d83dfb6c6
ArumetaM <matsuge.super.great@gmail.com>
2018-10-30 18:57:02 +0900

    Success

---
+++ ABC094C.py
@@ -0,0 +1,12 @@
+import copy
+N = int(input())
+X = list(map(int,input().split(" ")))
+Y = copy.deepcopy(X)
+Y.sort()
+B = (Y[N//2]+Y[N//2-1])/2
+
+for i in range(N):
+    if X[i] <= B:
+        print(Y[N//2])
+    else:
+        print(Y[N//2-1])
