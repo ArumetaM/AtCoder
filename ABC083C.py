1cc3f5efd195fbcaba386041095fb3b0a8623664
ArumetaM <matsuge.super.great@gmail.com>
2018-11-15 17:07:27 +0900

    Change some

--- ABC083C.py
+++ ABC083C.py
@@ -1,3 +1,11 @@
+"""
 import math
 X,Y = map(int,input().split(" "))
 print(int(math.log((Y/X),2)+1))
+"""
+X,Y = map(int,input().split(" "))
+ans = 0
+while X <= Y:
+    ans += 1
+    X *= 2
+print(ans)
